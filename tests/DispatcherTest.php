ffd4d3cdf7863f39ce044a1f1bc9fc412b7e4dd6
Grasseh <Steve.Gagnev4si@gmail.com>
2016-05-03 14:09:27 -0400

    Applied fixes from StyleCI

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -16,38 +16,37 @@ use PHPUnit_Framework_TestCase;
 
 class DispatcherTest extends PHPUnit_Framework_TestCase
 {
-
     public function testDispatcherProcedure()
     {
-        $test = "test";
-        $test2 = "test";
-        $closure = function() use (&$test){
-            $test .= "s";
+        $test = 'test';
+        $test2 = 'test';
+        $closure = function () use (&$test) {
+            $test .= 's';
         };
-        $closure2 = function() use (&$test2){
-            $test2 .= "v";
+        $closure2 = function () use (&$test2) {
+            $test2 .= 'v';
         };
         $dispatcher = new Dispatcher();
-        $dispatcher->listen('fire',$closure);
-        $dispatcher->listen('fire',$closure2);
+        $dispatcher->listen('fire', $closure);
+        $dispatcher->listen('fire', $closure2);
         $dispatcher->fire('fire');
-        $this->assertEquals($test,"tests");
-        $this->assertEquals($test2,"testv");
-        $dispatcher->listen('toast',$closure);
+        $this->assertEquals($test, 'tests');
+        $this->assertEquals($test2, 'testv');
+        $dispatcher->listen('toast', $closure);
         $dispatcher->flush('fire');
         $dispatcher->fire('fire');
         $dispatcher->fire('toast');
-        $this->assertEquals($test,"testss");
-        $this->assertEquals($test2,"testv");
+        $this->assertEquals($test, 'testss');
+        $this->assertEquals($test2, 'testv');
         $dispatcher->flush();
         $dispatcher->fire('toast');
-        $this->assertEquals($test,"testss");
-        $this->assertEquals($test2,"testv");
+        $this->assertEquals($test, 'testss');
+        $this->assertEquals($test2, 'testv');
     }
 
-    public function testEmptyFire(){
+    public function testEmptyFire()
+    {
         $dispatcher = new Dispatcher();
         $this->assertFalse($dispatcher->fire('test'));
     }
-
 }
