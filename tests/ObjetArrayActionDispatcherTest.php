3efe2a169d488b5b7f4c4c565892fce68ba332b7
Grasseh <Steve.Gagnev4si@gmail.com>
2016-05-03 15:15:36 -0400

    Applied fixes from StyleCI

--- tests/ObjetArrayActionDispatcherTest.php
+++ tests/ObjetArrayActionDispatcherTest.php
@@ -15,11 +15,14 @@ use Anekdotes\Meta\ObjectArrayActionDispatcher;
 use Anekdotes\Meta\Registry;
 use PHPUnit_Framework_TestCase;
 
-function test1(&$test){
+function test1(&$test)
+{
     $test .= 's';
 }
-class tester{
-    function test2(&$test){
+class tester
+{
+    public function test2(&$test)
+    {
         $test .= 't';
     }
 }
@@ -29,16 +32,17 @@ class ObjectArrayActionDispatcherTest extends PHPUnit_Framework_TestCase
     public function testObjectArrayDispatching()
     {
         $registry1 = new Registry();
-        $registry1->load(['Test' => 'Tests','Toast' => 'Toasts']);
+        $registry1->load(['Test' => 'Tests', 'Toast' => 'Toasts']);
         $registry2 = new Registry();
-        $registry2->load(['Test' => 'Nope','Toast' => 'Nope']);
-        $dispatcher = new ObjectArrayActionDispatcher([$registry1,$registry2]);
-        $dispatcher->set('Test','SuperTest');
+        $registry2->load(['Test' => 'Nope', 'Toast' => 'Nope']);
+        $dispatcher = new ObjectArrayActionDispatcher([$registry1, $registry2]);
+        $dispatcher->set('Test', 'SuperTest');
         $this->assertEquals($registry1->get('Test'), 'SuperTest');
         $this->assertEquals($registry2->get('Test'), 'SuperTest');
     }
 
-    public function testCallUserFuncArrayAndArrayDotGet(){
+    public function testCallUserFuncArrayAndArrayDotGet()
+    {
         $value = call_user_func_array('array_dot_get', [['toast' => 'toast'], null]);
         $this->assertEquals($value, ['toast' => 'toast']);
         $value = call_user_func_array('array_dot_get', [['toast' => 'toast'], 'toast']);
